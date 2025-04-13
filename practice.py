54c17111a2cef5932c0c644eb4308cfe2f18c5df
My Name <pradeepkjaiswal18@gmail.com>
2025-04-13 10:16:18 +0530

    test practice

---
+++ practice.py
@@ -0,0 +1,32 @@
+import datetime
+
+print ("Twinkle, twinkle, little star", "\n", "How I wonder what you are! Up above the world so high", "\n", "Like a diamond in the sky. Twinkle, twinkle, little star, "
+                                                                                                             "How I wonder what you are")
+
+print(datetime.datetime.now())
+
+def area(b):
+    area_circle= b*b
+    #print(area_circle)
+    return area_circle
+
+print(area(2))
+
+#reverse name
+def reverse(firstname, lastname):
+    lastnames=""
+    firstnames=""
+    lastnames=firstname
+    firstnames=lastname
+    print(firstnames, " ", lastnames)
+
+reverse("mike", "tyson")
+
+def tuplee(a,b,c,d):
+    list=[a,b,c,d]
+    tupll=(a,b,c,d)
+    return list, tupll
+
+print(tuplee(1,2,3,4))
+
+
